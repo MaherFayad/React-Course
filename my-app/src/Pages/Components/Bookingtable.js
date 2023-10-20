d1d07150ae32eb5665242b3069d80c0313fff25c
Maher Fayad <MaherFayad55@Gmail.com>
2023-10-21 02:43:31 +0300

    123

--- my-app/src/Pages/Components/Bookingtable.js
+++ my-app/src/Pages/Components/Bookingtable.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { fetchAPI, submitAPI } from './API';
-import '../../App.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { submitAPI } from "./API";
+import "../../App.css"; // Include your global CSS file here
 
-
-function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
-  const [resDate, setResDate] = useState('');
-  const [resTime, setResTime] = useState('17:00');
+function Bookingtable() {
+  const [resDate, setResDate] = useState("");
+  const [resTime, setResTime] = useState("17:00");
   const [guests, setGuests] = useState(1);
-  const [occasion, setOccasion] = useState('Birthday');
+  const [occasion, setOccasion] = useState("Birthday");
   const navigate = useNavigate();
 
   const isDateValid = (date) => {
-    return date.trim() !== '';
+    return date.trim() !== "";
   };
 
   const isGuestsValid = (guests) => {
@@ -26,10 +25,10 @@ function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
   };
 
   const clearForm = () => {
-    setResDate('');
-    setResTime('17:00');
+    setResDate("");
+    setResTime("17:00");
     setGuests(1);
-    setOccasion('Birthday');
+    setOccasion("Birthday");
   };
 
   const handleSubmit = async (e) => {
@@ -46,40 +45,39 @@ function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
       const isSubmitted = await submitAPI(formData);
 
       if (isSubmitted) {
-        navigate('/confirmed');
-        alert('Reservation created!');
+        navigate("/confirmed");
+        alert("Reservation created!");
         clearForm();
       } else {
-        alert('Reservation submission failed.');
+        alert("Reservation submission failed.");
       }
     } catch (error) {
-      console.error('Error submitting reservation:', error);
-      alert('Reservation submission failed.');
+      console.error("Error submitting reservation:", error);
+      alert("Reservation submission failed.");
     }
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96 space-y-4">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-6">Make a Reservation</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
+    <div className="modal-container">
+      <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="custom-booking-form"> {/* Apply custom class for the component */}
+        <h1 className="custom-heading">Make a Reservation</h1>
+        <form onSubmit={handleSubmit} className="custom-form">
           <div>
-            <label htmlFor="res-date" className="text-sm text-gray-600 font-semibold">Choose Date</label>
+            <label htmlFor="res-date">Choose Date</label>
             <input
               type="date"
               id="res-date"
               value={resDate}
               onChange={(e) => setResDate(e.target.value)}
-              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:ring focus:ring-indigo-200 focus:outline-none"
             />
           </div>
           <div>
-            <label htmlFor="res-time" className="text-sm text-gray-600 font-semibold">Choose Time</label>
+            <label htmlFor="res-time">Choose Time</label>
             <select
               id="res-time"
               value={resTime}
               onChange={(e) => setResTime(e.target.value)}
-              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:ring focus:ring-indigo-200 focus:outline-none"
             >
               <option value="17:00">17:00</option>
               <option value="18:00">18:00</option>
@@ -90,7 +88,7 @@ function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
             </select>
           </div>
           <div>
-            <label htmlFor="guests" className="text-sm text-gray-600 font-semibold">Number of Guests</label>
+            <label htmlFor="guests">Number of Guests</label>
             <input
               type="number"
               placeholder="1"
@@ -99,16 +97,14 @@ function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
               id="guests"
               value={guests}
               onChange={(e) => setGuests(parseInt(e.target.value))}
-              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:ring focus:ring-indigo-200 focus:outline-none"
             />
           </div>
           <div>
-            <label htmlFor="occasion" className="text-sm text-gray-600 font-semibold">Occasion</label>
+            <label htmlFor="occasion">Occasion</label>
             <select
               id="occasion"
               value={occasion}
               onChange={(e) => setOccasion(e.target.value)}
-              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:ring focus:ring-indigo-200 focus:outline-none"
             >
               <option value="Birthday">Birthday</option>
               <option value="Anniversary">Anniversary</option>
@@ -118,13 +114,14 @@ function Bookingtable({ availableTimes, dispatch, bookingDataState }) {
             <button
               type="submit"
               disabled={!getIsFormValid()}
-              className="w-full bg-indigo-600 text-white rounded-md font-semibold py-2 hover:bg-indigo-700 focus:ring focus:ring-indigo-200 focus:outline-none"
+              className="custom-button" // Apply a custom class for the button
             >
               Make Your Reservation
             </button>
           </div>
         </form>
       </div>
+    </div>
     </div>
   );
 }
